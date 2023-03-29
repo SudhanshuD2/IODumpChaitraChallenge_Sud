44fe1fb3c1729f30464591129de129f448a16b52
Sudhanshu Dixit <128416185+SudhanshuD2@users.noreply.github.com>
2023-03-29 18:12:33 +0530

    Create Day6.py

---
+++ Day6.py
@@ -0,0 +1,9 @@
+# CODE
+F = int(input("Enter temprature in Fahrenheight: "))
+D = (F-32)*(5/9)
+D = round(D, 2)
+print("Temprature in Degree celcius: {}".format(D))
+
+#INPUT and OUTPUT
+# Enter temprature in Farenheit: 15
+# Temprature in Degree Celcius: -9.44
